71ee3a287f0adc032746726d89869d7fa8c30625
Manish K <manishkuniyal24@gmail.com>
2024-02-21 23:59:37 +0530

    Dependencies

--- Server/index.js
+++ Server/index.js
@@ -1,1 +1,14 @@
-console.log('okok')
+const express= require('express')
+const app = express()
+app.use(express.json())
+app.get('/',(req,res)=>{
+  res.send('okok')
+})
+
+app.post('/run',(req,res)=>{
+  res.json(req.body)
+})
+
+app.listen(4000,()=>{
+  console.log('mio')
+})
