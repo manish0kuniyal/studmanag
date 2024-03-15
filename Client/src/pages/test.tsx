3810c44e0d76edb2c406569117230b864c95893a
Manish K <manishkuniyal24@gmail.com>
2024-03-15 23:53:20 +0530

    Alternate logic for rendering tables

--- Client/src/pages/test.tsx
+++ Client/src/pages/test.tsx
@@ -1,93 +1,59 @@
-// import React, { useState } from 'react'
-// import * as XLSX from "xlsx"
-// function Test() {
-// const [excelFile,setExcelFile]=useState(null)
-// const [excelData,setExcelData]=useState(null)
+import React, { useState } from "react";
+import Papa from "papaparse";
 
-// const handleFileChange=(e)=>{
-//         let selectedFile = e.target.files[0]    
-//         if(selectedFile){
-//         console.log(selectedFile.type)
-//         let reader=new FileReader()
-//         reader.readAsArrayBuffer(selectedFile)
-//         reader.onload=(e)=>{
-//             setExcelFile(e.target.result)
-//         }
-//         }
-//         else{
-//             console.log("select a file")
-//         }
-// }
+const Test = () => {
+    const [data, setData] = useState([]);
 
-// const handleFileSubmit=(e)=>{
-//     e.preventDefault()
-//     if(excelFile!=null){
-//         const workbook=XLSX.read(excelFile,{type:"buffer"})
-//         const worksheetName=workbook.SheetNames[0]
-//         const worksheet=workbook.Sheets[worksheetName]
-//         const data=XLSX.utils.sheet_to_json(worksheet)
-//         setExcelData(data.slice(0,10))
-//     }
-// }
-
-// return (<>
-//     <div>Test</div>
-//     <form className='border-2 flex flex-col'
-//     onSubmit={handleFileSubmit}
-//     >
-//     <input
-//     type="file"
-//     onChange={handleFileChange}
-    // accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel"
-//         />    
-//     <button className='border-2 m-2 w-[20%]' onClick={handleFileChange}>Upload</button>
-//     </form>
-//     <div>
-//         {excelData ?(
-//         <div>
-//             <table>
-//                 <thead>
-//                     <tr>
-//                         {Object.keys(excelData[0]).map((key)=>{
-//                             <th key={key}>{key}</th>
-//                         })}
-//                     </tr>
-//                 </thead>
-//             </table>
-//         </div>
-//         ):
-//         (
-//         <div>No File is Uploaded</div>
-//         )}
-//     </div>
-    
-//     </> )
-// }
-
-// export default Test 
-
-import React from 'react'
-import Papa from 'papaparse'
-function Test() {
-
-    const handleFile =(event)=>{
-            Papa.parse(event.target.files[0],{
-                header:true,
-                skipEmptyLines:true,
-                complete:function(result){
-                     
+    const handleFileUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const file = e.target.files?.[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = (event) => {
+                if (event.target) {
+                    const result = event.target.result as string;
+                    Papa.parse(result, {
+                        header: true,
+                        encoding: "UTF-8",
+                        complete: (results) => {
+                            setData(results.data);
+                        }
+                    });
                 }
-            })
-    }
-  return (<>
-    <div>Test</div>
-    <input
-    type='file'
-    name='file'
-    accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel"
-    style={{display:"block",margin:"10px auto"}}
-    />
-    </>  )
-}
+            };
+            reader.readAsText(file, "UTF-8");
+        }
+    };
 
-export default Test
+    return (
+        <>
+            <div>UPLOAD A CSV</div>
+            <input
+                type="file"
+                accept=".csv, .xml, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel"
+                onChange={handleFileUpload}
+            />
+            {data.length > 0 && (
+                <table>
+                    <thead>
+                        <tr>
+                            {Object.keys(data[0]).map((header, index) => (
+                                <th key={index}>{header}</th>
+                            ))}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data.map((row, rowIndex) => (
+                            <tr key={rowIndex}>
+                                {Object.values(row).map((value, columnIndex) => (
+                                    <td key={columnIndex}>{value}</td>
+                                ))}
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            )}
+        </>
+    );
+};
+
+export default Test;
