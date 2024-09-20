4000e68e5ffff807d6558c4aacfca35e45c073d0
manish0kuniyal <manishkuniyal24@gmail.com>
2024-09-20 22:17:41 +0530

    finalcommit

---
+++ csvBot.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+om langchain_community.agent_toolkits import create_sql_agent
+from langchain_openai import ChatOpenAI
+
+
+df = pd.read_csv("titanic.csv")
+print(df.shape)
+print(df.columns.tolist())
+
+
+llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
+agent_executor = create_sql_agent(llm, db=db, agent_type="openai-tools", verbose=True)
